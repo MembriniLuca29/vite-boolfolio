26569d084acbee40bc59dc00ad5e731660d91ddd
MembriniLuca29 <lucamembrini29@gmail.com>
2023-09-27 17:16:48 +0200

    milestone 3

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import MainComponent from './components/MainComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+  data(){
+    return {
+      
+    }
+  },
+  components: {
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+  }
+}
+</script>
+
+<template>
+
+ <HeaderComponent/>
+
+ <MainComponent/>
+
+ <FooterComponent/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            posts: []
+        }
+    },
+    created() {
+        axios
+        .get('http://localhost:8000/api/posts')
+        .then(res =>{
+            console.log(res)
+        });
+
+    }
+}
+</script>
+
+<template>
+ <h1>ciao main</h1>
+</template>
+
+<style scoped>
+
+</style>
